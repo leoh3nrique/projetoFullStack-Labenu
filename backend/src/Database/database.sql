29532a0c8d698f8d1c54e9b4e3d9d92c6e00a590
Leonardo Tiburcio <hgtleohgt@gmail.com>
2023-07-08 12:14:50 -0300

    models de Post e User feitos

---
+++ backend/src/Database/database.sql
@@ -0,0 +1,40 @@
+-- Active: 1688827975896@@127.0.0.1@3306
+
+CREATE TABLE
+    users(
+        id TEXT NOT NULL UNIQUE PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TEXT DEFAULT(DATETIME()) NOT NULL
+    );
+
+CREATE TABLE
+    posts(
+        id TEXT NOT NULL UNIQUE PRIMARY KEY,
+        creator_id TEXT NOT NULL,
+        content TEXT NOT NULL,
+        likes INT(0),
+        dislikes INT(0),
+        comments INT(0),
+        created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+        update_at TEXT DEFAULT(DATETIME()) NOT NULL,
+        Foreign Key (creator_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    likes_dislikes(
+        user_id TEXT NOT NULL,
+        post_id TEXT NOT NULL,
+        like INTEGER NOT NULL,
+        Foreign Key (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE Foreign Key (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    comments(
+        user_id TEXT NOT NULL,
+        post_id TEXT NOT NULL,
+        content TEXT NOT NULL,
+        Foreign Key (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE Foreign Key (post_id) REFERENCES posts(id) ON UPDATE CASCADE ON DELETE CASCADE
+    )
---
+++ backend/src/models/Post.ts
@@ -0,0 +1,109 @@
+export interface PostDb {
+    id: string,
+    creator_id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    comments: number,
+    created_at: string,
+    update_at: string
+}
+
+export class Post {
+    constructor(
+        private id: string,
+        private content: string,
+        private likes: number,
+        private dislikes: number,
+        private comments: number,
+        private createdAt: string,
+        private updateAt: string,
+        private creatorId: string,
+        private creatorName: string
+    ) { }
+
+    public getId(): string {
+        return this.id
+    }
+    public setId(value: string): void {
+        this.id = value
+    }
+    public getCreatorId(): string {
+        return this.creatorId
+    }
+    public setCreatorId(value: string): void {
+        this.creatorId = value
+    }
+    public getCreatorName(): string {
+        return this.creatorName
+    }
+    public setCreatorName(value: string): void {
+        this.creatorName = value
+    }
+    public getContent(): string {
+        return this.content
+    }
+    public setContent(value: string): void {
+        this.content = value
+    }
+    public getLikes(): number {
+        return this.likes
+    }
+    public setLikes(value: number): void {
+        this.likes = value
+    }
+    public getDislikes(): number {
+        return this.dislikes
+    }
+    public setDislikes(value: number): void {
+        this.dislikes = value
+    }
+    public getComments(): number {
+        return this.comments
+    }
+    public setComments(value: number): void {
+        this.comments = value
+    }
+    public getCreatedAt(): string {
+        return this.createdAt
+    }
+    public setCreatedAt(value: string): void {
+        this.createdAt = value
+    }
+    public getUpdatedAt(): string {
+        return this.updateAt
+    }
+    public setUpdatedAt(value: string): void {
+        this.updateAt = value
+    }
+    public toDBModel() {
+        return {
+            id: this.id,
+            creator_id: this.creatorId,
+            content: this.content,
+            likes: this.likes,
+            dislikes: this.dislikes,
+            comments: this.comments,
+            created_at: this.createdAt,
+            update_at: this.updateAt
+        }
+    }
+
+    public toModel() {
+        return {
+            id: this.id,
+            content: this.content,
+            likes: this.likes,
+            dislikes: this.dislikes,
+            comments: this.comments,
+            createdAt: this.createdAt,
+            updateAt: this.updateAt,
+            creator: {
+                creatorId: this.creatorId,
+                creatorName: this.creatorName
+            }
+
+        }
+    }
+}
+
